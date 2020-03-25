bda57e84f096b8cdedd37908a82e080537349d60
luododo <1619102340@qq.com>
2020-03-25 17:00:49 +0800

    第一次项目经理

---
+++ src/main/java/com/ithr/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.ithr.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author com.hr.Text
+ * @date 2020/3/25 - 13:36
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("info")
+    public  String info(){
+        return "hello git01";
+    }
+}
